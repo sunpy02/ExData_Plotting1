e68495958b2338f993565ce1d579f51bd4e1859b
sunpy02 <sunpy02@GMAIL.COM>
2014-05-09 15:00:05 -0400

    Create Plot1.r

---
+++ Plot1.r
@@ -0,0 +1,7 @@
+library(sqldf)
+DF<- read.csv.sql("household_power_consumption.txt", header=T, sep=";", sql = 'select * from file where Date = "1/2/2007" or Date="2/2/2007"')
+DF$Date<-as.Date(DF$Date,"%d/%m/%Y")
+DF$Time<-strptime(paste(DF$Date,DF$Time),"%Y-%m-%d %H:%M:%S")
+png(file="plot1.png",width = 480, height = 480, units = "px")
+hist(DF$Global_active_power,xlab="Global Active Power (kilowatts)",main="Global Active Power",col="red")
+dev.off()
